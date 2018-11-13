59adffa36b51aac537ac32327744de8d61c591d5
Michael Ambrus <michael@helsinova.se>
2018-11-13 11:41:14 +0100

    Porting changes to pass build

--- service_terminal.c
+++ service_terminal.c
@@ -5,14 +5,14 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
-#include "argutil.h"
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#include <argutil.h>
 #include <pthread.h>
-#include "luabind.h"
+#include <lua/console.h>
+#include <tcp-tap/server.h>
 #include "service_terminal.h"
-#include "server.h"
 #undef  NDEBUG
 #include <assert.h>
 
